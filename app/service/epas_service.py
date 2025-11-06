c720a00486230fc214567c0827bbdab2b8da62d5
mix <mixfoudev@gmail.com>
2025-11-06 10:13:32 +0200

    Clean up

--- app/service/epas_service.py
+++ app/service/epas_service.py
@@ -4,22 +4,6 @@ from app.service import static_data_service as staticService
 from app import util as u
 import uuid
 
-def save2(user, contact, personal, student, classStud):
-    eduId = staticService.get_edu_id(student.get('edu'))
-    eduSpecId = staticService.get_edu_year_spec(student.get('edu'), classStud.get('acYear'), classStud.get('spec'))
-
-    if not eduId or not eduSpecId:
-        print(f"epas save: can not find eduId : {eduId} or eduSpecId: {eduSpecId}. Aborting")
-
-    userId = s.getOrInsertUser(user)
-    contactId = s.createContact(contact)
-    personalId = s.createPersonal(personal)
-    print("user id: ", userId)
-    print("contact id: ", contactId)
-    print("personal id: ", personalId)
-    srUuid = str(uuid.uuid4())
-    fieldsId = s.createStudentFields(student, userId, None, contactId, personalId, 1, eduSpecId, eduId, srUuid)
-    print("fieldsId : ", fieldsId)
 
 def save(user, contact, personal, student, classStud):
     db = current_app.config['DB']
